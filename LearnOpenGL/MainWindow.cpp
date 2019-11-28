f813bea472f3d03f8211c5ebdac7454608503360
KhanhDuy <khanhduyn84@gmail.com>
2019-11-28 20:00:16 +0700

    add GLSLShader class & render triangle with more attribute

--- LearnOpenGL/MainWindow.cpp
+++ LearnOpenGL/MainWindow.cpp
@@ -1,24 +1,7 @@
 #include "MainWindow.h"
+#include "GLSLShader.h"
 #include <iostream>
 #include "define.h"
-
-const char* vertexShaderSources = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 OurColor;\n"
-"void main()\n"
-"{\n"
-"gl_Position = vec4(aPos, 1.0);\n"
-"OurColor = aColor;"
-"}\0";
-
-const char* fragmentShaderSources = "#version 330 core\n"
-"in vec3 OurColor;"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"FragColor = vec4(OurColor, 1.0f);\n"
-"}\0";
 
 void MainWindow::framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -96,75 +79,30 @@ void MainWindow::ProcessInput()
 void MainWindow::Run()
 {
 	float vertices[] = {
-		-0.5f,-0.5f,
-		0.5f,-0.5f,
-		0.0f,0.5f
+		//Position					//Color
+		-0.5f, -0.5f, 0.0f,			1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,			0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f,			0.0f, 0.0f, 1.0f
 	};
 	
-	float colors[] = {
-		0.0f, 0.8f ,0.8f,
-		0.0f, 0.8f, 0.8,
-		1.0f, 0.0f, 0.0f
-	};
-	GLuint VAO, VBO, VBO1;
+	GLuint VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &VBO1);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	int success;
-	char infoLog[512];
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSources, nullptr);
-	glCompileShader(vertexShader);
-
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << std::endl;
-		std::cout << infoLog << std::endl;
-	}
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSources, nullptr);
-	glCompileShader(fragmentShader);
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << std::endl;
-		std::cout << infoLog << std::endl;
-	}
-
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED" << std::endl;
-		std::cout << infoLog << std::endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	std::unique_ptr<GLSLShader> triangleShader = std::make_unique<GLSLShader>("../Resources/Shaders/TriangleShaderVS.vs","../Resources/Shaders/TriangleShaderFS.fs");
 
 	float deltaTime = 0.0f;
 	float lastFrame = 0.0f;
@@ -179,8 +117,8 @@ void MainWindow::Run()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		ProcessInput();
-
-		glUseProgram(shaderProgram);
+		
+		triangleShader->Use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
